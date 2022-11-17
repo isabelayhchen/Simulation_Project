67c4d66fdfbdb48eae26c79848c2fbb80db1aeb6
isabelayhchen <isabelayhchen@berkeley.edu>
2022-11-17 13:59:18 -0800

    add passenger class

---
+++ Simulation_Project/Overhead_Compartment.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class Passenger:
+
+    num_of_bins = 25
+
+    def __init__(self):
+        self.num_of_bins = 25
+        self.bins = np.array([self.num_of_bins])
+
+
+    def 
+
